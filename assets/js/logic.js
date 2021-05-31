9fc3185e1c15d1cae6f5e5f78f586d65c0b8eb2a
Steve <steve.pharand@gmail.com>
2021-05-31 11:32:27 -0400

    Démarrage du projet

---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+
+    // function update_switches()
+    // {
+    //     const http = new XMLHttpRequest();
+    //     const url='http://192.168.1.10/cmd/sws';
+
+    //     http.open("GET", url);
+    //     http.send();
+
+    //     http.onreadystatechange = function() {
+    //         console.log("Switch state: " + http.responseText);
+    //         document.getElementById("switches").innerHTML = http.responseText;
+    //     }
+    // }
+
+
+    // window.setInterval(update_switches, 1000);
+
