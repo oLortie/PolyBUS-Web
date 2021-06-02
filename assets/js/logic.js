2828506b536ad36475d99d202a48adcd3fa6b504
Zachary Thuotte <zacharythuotte@hotmail.com>
2021-06-02 01:01:20 -0400

    Added graph logic and UI plot for respiration example with Plotly

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,48 @@
 
-    // function update_switches()
-    // {
-    //     const http = new XMLHttpRequest();
-    //     const url='http://192.168.1.10/cmd/sws';
+// function update_switches()
+// {
+//     const http = new XMLHttpRequest();
+//     const url='http://192.168.1.10/cmd/sws';
 
-    //     http.open("GET", url);
-    //     http.send();
+//     http.open("GET", url);
+//     http.send();
 
-    //     http.onreadystatechange = function() {
-    //         console.log("Switch state: " + http.responseText);
-    //         document.getElementById("switches").innerHTML = http.responseText;
-    //     }
-    // }
+//     http.onreadystatechange = function() {
+//         console.log("Switch state: " + http.responseText);
+//         document.getElementById("switches").innerHTML = http.responseText;
+//     }
+// }
 
 
-    // window.setInterval(update_switches, 1000);
+// window.setInterval(update_switches, 1000);
+
+var graphDiv = document.getElementById('graph');
+
+function createGraph(){
+
+    var layout = {
+        title: 'Respiration de l\'individu',
+        xaxis: {
+          title: 't (s)',
+          showgrid: true
+        },
+        yaxis: {
+          title: 'VOLUME POUMONS? (A CHANGER)',
+          showline: false
+        }
+      };
+
+    Plotly.newPlot(graphDiv, [{y:[], type: 'line', line:{shape: 'spline'}}], layout);
+}
+
+function extendGraph(valueY){
+
+    Plotly.extendTraces(graphDiv, {y:[[valueY]]}, [0]);
+}
+
+//----------------------------------------------------------------
+
+createGraph();
+setInterval(function() { extendGraph(Math.random()); }, 100);
+// setInterval( function() { funca(10,3); }, 500 );
 
