8c22071702f90da40aac231f2f4e739069e69524
Steve <steve.pharand@gmail.com>
2021-06-03 03:09:32 -0400

    Change View and add graphs for visualization

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,10 +16,12 @@
 
 // window.setInterval(update_switches, 1000);
 
-var graphDiv = document.getElementById('graph');
 
-function createGraph(){
+//-------------------------------------------------------------------------------------------------------------------------------
+// Function for respiration Graph
+var graph_respiration = document.getElementById('graph_respiration');
 
+function createGraphRespiration(){
     var layout = {
         title: 'Respiration de l\'individu',
         xaxis: {
@@ -31,17 +33,14 @@ function createGraph(){
           showline: false
         }
       };
-
-    Plotly.newPlot(graphDiv, [{y:[], type: 'scatter'}], layout);
+    Plotly.newPlot(graph_respiration, [{y:[], type: 'scatter'}], layout);
 }
 
 function extendGraph(valueY){
-
-    Plotly.extendTraces(graphDiv, {y:[[valueY]]}, [0]);
+    Plotly.extendTraces(graph_respiration, {y:[[valueY]]}, [0]);
 }
 
-function update_respiration()
-{
+function update_respiration(){
     const http = new XMLHttpRequest();
     const url='http://192.168.1.10/cmd/respiration';
 
@@ -50,21 +49,80 @@ function update_respiration()
     var resp_obj;
     var respi;
     
-    http.onreadystatechange = function() 
-    {
+    http.onreadystatechange = function() {
         console.log("Respiration voltage: " + http.responseText);
         document.getElementById("respiration").innerHTML = http.responseText;
         resp_obj = JSON.parse(http.responseText);
         respi = resp_obj.respiration;
-        Plotly.extendTraces(graphDiv, {y:[[respi]]}, [0]);
+        Plotly.extendTraces(graph_respiration, {y:[[respi]]}, [0]);
         console.log(respi)
     }
-    
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
+// Function for Graph X
+var graph_X = document.getElementById('graph_X');
+
+function createGraphX(){
+    var layout = {
+        title: 'X',
+        xaxis: {
+          title: 't (ms)',
+          showgrid: true
+        },
+        yaxis: {
+          title: 'Volume Air Poumon',
+          showline: false
+        }
+      };
+    Plotly.newPlot(graph_X, [{y:[], type: 'scatter'}], layout);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
+// Function for Graph Y
+var graph_Y = document.getElementById('graph_Y');
+
+function createGraphY(){
+    var layout = {
+        title: 'Y',
+        xaxis: {
+          title: 't (ms)',
+          showgrid: true
+        },
+        yaxis: {
+          title: 'Volume Air Poumon',
+          showline: false
+        }
+      };
+    Plotly.newPlot(graph_Y, [{y:[], type: 'scatter'}], layout);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------
+// Function for Graph Z
+var graph_Z = document.getElementById('graph_Z');
+
+function createGraphZ(){
+    var layout = {
+        title: 'Z',
+        xaxis: {
+          title: 't (ms)',
+          showgrid: true
+        },
+        yaxis: {
+          title: 'Volume Air Poumon',
+          showline: false
+        }
+      };
+    Plotly.newPlot(graph_Z, [{y:[], type: 'scatter'}], layout);
 }
 
 //----------------------------------------------------------------
 
-createGraph();
+createGraphRespiration();
+createGraphX();
+createGraphY();
+createGraphZ();
+
 setInterval(function() { update_respiration(); }, 100);
 // setInterval( function() { funca(10,3); }, 500 );
 
