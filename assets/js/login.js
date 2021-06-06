0eeada36fa4721022044d3045aa0c89305c30a9b
Charles <charles.matte03@gmail.com>
2021-06-05 22:30:49 -0400

    [UI] added creation page plus navbar

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,12 @@
 function validate()
 {
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
     if(username == "admin" && password == "user"){
         console.log("Allo");
-        window.location.replace = "../graph/index.html";
-       return false;
-        
+        window.location.replace("../graph/index.html");
+        return false;
     }
 
     else{
