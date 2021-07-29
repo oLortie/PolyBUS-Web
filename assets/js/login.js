61fa2f1b9046a0ba6648ea0a42a3d58b499db58b
Charles <charles.matte03@gmail.com>
2021-07-29 10:59:10 -0400

    [UI] Change login

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,7 @@ function validate()
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if(username == "admin" && password == "user"){
+    if(username == "admin" && password == "admin"){
         console.log("Allo");
         window.location.replace("../graph/index.html");
     }
